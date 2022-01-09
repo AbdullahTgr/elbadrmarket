8ca463b1ea46df15ee2e73a638883dcfc7e1c99b
AbdullahTgr <4multijob@gmail.com>
2022-01-09 14:59:51 +0200

    visitors

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -77,7 +77,7 @@ class AdminController extends Controller
         $todat_visitorcount = visitors::where('created_at','like','%'.substr(date('Y-m-d H:i:s'),0,10).'%')->get()->count();
         $visitorcount = visitors::get()->count();
 
-        return view('management.index', compact('visitorcount','todat_visitorcount','productsnum','todayproducts','hme','setups','prd','service','about','portfolio','team','contactus'));
+        return view('management.index', compact('visitorcount','todat_visitorcount','usernum','productsnum','todayproducts','hme','setups','prd','service','about','portfolio','team','contactus'));
     }
 
 
